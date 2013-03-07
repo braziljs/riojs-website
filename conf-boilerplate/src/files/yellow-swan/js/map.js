15d6abd22308f5b32096a9201fc8e6d04bbd9278
Leonardo Alberto Souza <leobetosouza@gmail.com>
2013-03-07 03:51:27 -0300

    corrigindo mapa e retirando codigo inutil #45

--- conf-boilerplate/src/files/yellow-swan/js/map.js
+++ conf-boilerplate/src/files/yellow-swan/js/map.js
@@ -1,54 +1,26 @@
-var geocoder, map, google, icon = 'yellow-swan/img/marker-default.png';
-
-function initialize() {
-  'use strict';
-
-  geocoder = new google.maps.Geocoder();
-
-  var latlng = new google.maps.LatLng(0, 0),
-    myOptions = {
-      zoom: 16,
-      center: latlng,
-      scrollwheel: false,
-      streetViewControl: true,
-      labels: true,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-  map = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
-  map.setCenter(latlng);
-}
-
-$(document).ready(function() {
+$(function() {
 
   'use strict';
 
-  $('#map-canvas').each(function() {
+  var icon = 'yellow-swan/img/marker-default.png';
 
-    var address = $(this).attr('data-address');
+  var unirio = new google.maps.LatLng(-22.954936, -43.168684);
+  
+  var myOptions = {
+    zoom: 16,
+    center: unirio,
+    scrollwheel: false,
+    streetViewControl: true,
+    labels: true,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
 
-    geocoder = new google.maps.Geocoder();
+  var map = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
 
-    initialize();
-
-    geocoder.geocode({
-      'address': address
-    }, function(results, status) {
-
-      if (status === google.maps.GeocoderStatus.OK) {
-
-        map.setCenter(results[0].geometry.location);
-
-        new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location,
-          icon: icon
-        });
-      } else {
-        if (console) {
-          console.log('Google Maps was not loaded: ', status);
-        }
-      }
-    });
+  var pin = new google.maps.Marker({
+    map: map,
+    position: unirio,
+    icon: icon
   });
+
 });
